454a3f774ec4d072ebbe83e9aae2142ed52b09b6
DmitryDemenkov <demon.krut2002@mail.ru>
2023-03-31 18:59:10 +0300

    Wall class implemented

--- main/src/model/Wall.java
+++ main/src/model/Wall.java
@@ -1,13 +1,25 @@
 package model;
 
-public class Wall extends Obstacle{
+import events.ObjectInCellEvent;
+
+public class Wall extends Obstacle implements Damageable{
+
+    private boolean isDestroyed = false;
+
     @Override
     public void faceWith(ObjectInCell object) {
+        super.faceWith(object);
 
+        if (object instanceof Bullet){
+            isDestroyed = true;
+            fireEvent(new ObjectInCellEvent(this, ObjectInCellEvent.EventType.DESTROYING));
+        }
     }
 
     @Override
     void update() {
-
+        if (isDestroyed){
+            getCell().takeObject(this);
+        }
     }
 }
