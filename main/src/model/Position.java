c8cba1ffb0e3e5cbfee8e8da2fcabb8acf20f6b4
DmitryDemenkov <demon.krut2002@mail.ru>
2023-03-31 18:40:40 +0300

    position class implemented

--- main/src/model/Position.java
+++ main/src/model/Position.java
@@ -2,18 +2,46 @@ package model;
 
 public class Position {
 
+    private final int _x;
+
     public int getX() {
-        return 0;
+        return _x;
     }
 
+    private final int _y;
+
     public int getY(){
-        return 0;
+        return _y;
     }
 
     public Position(int x, int y){
+        _x = x;
+        _y = y;
     }
 
     public Position shift(Direction direction, int delta){
-        return null;
+        int xDirection = 0;
+        int yDirection = 0;
+
+        switch (direction){
+            case NORTH -> yDirection = -1;
+            case SOUTH -> yDirection = 1;
+            case WEST -> xDirection = -1;
+            case EAST -> xDirection = 1;
+        }
+
+        int newX = getX() + xDirection * delta;
+        int newY = getY() + yDirection * delta;
+
+        return new Position(newX, newY);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Position other)){
+            return false;
+        }
+
+        return this.getX() == other.getX() && this.getY() == other.getY();
     }
 }
