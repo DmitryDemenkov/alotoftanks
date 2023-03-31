ac75a8ac77e5bcd4a8e1ecac306dc82f6be8dd00
DmitryDemenkov <demon.krut2002@mail.ru>
2023-03-31 19:00:28 +0300

    Headquarters class implemented

--- main/src/model/Headquarters.java
+++ main/src/model/Headquarters.java
@@ -1,25 +1,45 @@
 package model;
 
-public class Headquarters extends ObjectInCell{
+import events.ObjectInCellEvent;
+
+public class Headquarters extends ObjectInCell implements Damageable {
+
+    private boolean isDestroying = false;
+
+    private Tank _tank;
 
     public Tank getTank(){
-        return null;
+        return _tank;
     }
 
-    public void setTank(Tank tank){ }
+    public void setTank(Tank tank){
+        if (tank == _tank){
+            return;
+        }
+
+        _tank = tank;
+        tank.setHeadquarters(this);
+    }
 
     @Override
     public boolean canFaceWith(ObjectInCell object) {
-        return false;
+        return object instanceof Bullet;
     }
 
     @Override
     public void faceWith(ObjectInCell object) {
+        super.faceWith(object);
 
+        if (object instanceof Bullet){
+            isDestroying = true;
+            fireEvent(new ObjectInCellEvent(this, ObjectInCellEvent.EventType.DESTROYING));
+        }
     }
 
     @Override
     void update() {
-
+        if (isDestroying){
+            getCell().takeObject(this);
+        }
     }
 }
