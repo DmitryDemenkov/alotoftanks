dfc4a37ccc9d9015711a6331515f415913b0e7b3
DmitryDemenkov <demon.krut2002@mail.ru>
2023-04-02 18:06:49 +0300

    Bullet class implemented

--- main/src/model/Bullet.java
+++ main/src/model/Bullet.java
@@ -1,21 +1,48 @@
 package model;
 
+import events.ObjectInCellEvent;
+
 public class Bullet extends MovableObject{
 
-    public Bullet(Direction direction, Cell startCell){ }
+    private boolean isDestroying = false;
+
+    public Bullet(Direction direction, Cell startCell){
+        setDirection(direction);
+        setCell(startCell);
+    }
 
     @Override
     public boolean canFaceWith(ObjectInCell object) {
-        return false;
+        return !(this == object);
     }
 
     @Override
     public void faceWith(ObjectInCell object) {
+        super.faceWith(object);
 
+        if (object instanceof Damageable){
+            isDestroying = true;
+            fireEvent(new ObjectInCellEvent(this, ObjectInCellEvent.EventType.DESTROYING));
+        }
     }
 
     @Override
     void update() {
+        if (isDestroying){
+            getCell().takeObject(this);
+        } else {
+            move();
+        }
+    }
 
+    @Override
+    public boolean move(){
+        boolean isMoved = super.move();
+        if (!isDestroying && isMoved){
+            fireEvent(new ObjectInCellEvent(this, ObjectInCellEvent.EventType.MOVING));
+        } else if (!isMoved){
+            getCell().takeObject(this);
+        }
+        return isMoved;
     }
 }
