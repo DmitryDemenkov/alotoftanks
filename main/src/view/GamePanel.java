3ee59720ed91f92680b631a9efa13ab5181b527d
DmitryDemenkov <demon.krut2002@mail.ru>
2023-04-23 12:17:16 +0300

    added game panel

---
+++ main/src/view/GamePanel.java
@@ -0,0 +1,101 @@
+package view;
+
+import events.IGameEventListener;
+import events.ObjectInCellEvent;
+import model.Game;
+import model.Tank;
+import model.environment.SimpleEnvironment;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GamePanel extends JFrame {
+
+    private final WidgetPool _pool = new WidgetPool();
+
+    private final List<PlayerPanel> _playerPanels = new ArrayList<>();
+
+    private Game _game;
+
+    public GamePanel(){
+        create();
+        pack();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
+        start();
+    }
+
+    private void create(){
+        _game = new Game(new SimpleEnvironment());
+        FieldWidget fieldWidget = new FieldWidget(_game.getField(), _pool);
+        _game.addListener(new GameListener());
+
+        JPanel content = (JPanel) getContentPane();
+        content.removeAll();
+        content.setLayout(new BoxLayout(content, BoxLayout.X_AXIS));
+
+        for (Tank tank : _game.getField().getTanks()){
+            PlayerPanel panel = new PlayerPanel((TankWidget)_pool.getWidget(tank));
+            content.add(panel);
+            _playerPanels.add(panel);
+        }
+
+        content.add(fieldWidget, 1);
+
+        repaint();
+        revalidate();
+    }
+
+    private void start(){
+        _game.start();
+        ((TankWidget) _pool.getWidget(_game.activeTank())).setActive(true);
+    }
+
+    private void restart(){
+        create();
+        start();
+    }
+
+    private class GameListener implements IGameEventListener {
+
+        @Override
+        public void onObjectChanged(ObjectInCellEvent event) {
+            ObjectInCellWidget widget = _pool.getWidget(event.getObject());
+            if (event.getType() == ObjectInCellEvent.EventType.MOVING && widget instanceof MovableObjectWidget movableWidget){
+                CellWidget cellWidget = event.getObject().getCell() != null ? _pool.getWidget(event.getObject().getCell()) : null;
+                movableWidget.moveTo(cellWidget);
+            } else{
+                widget.getDamage();
+                if (event.getType() == ObjectInCellEvent.EventType.DESTROYING){
+                    _pool.removeWidget(event.getObject());
+                }
+            }
+        }
+
+        @Override
+        public void onGameStateChanged(Game.State state) {
+            for (PlayerPanel panel: _playerPanels ) {
+                panel.update();
+            }
+
+            if (state == Game.State.GAME_IS_ON){
+                ((TankWidget) _pool.getWidget(_game.activeTank())).setActive(true);
+            } else if (state == Game.State.WINNER_FOUND){
+                String winnerName = getColorName(((TankWidget)_pool.getWidget(_game.winner())).getColor());
+                JOptionPane.showMessageDialog(GamePanel.this, winnerName + " танк победил");
+                restart();
+            }
+        }
+
+        private String getColorName(Color color){
+            if (color == Color.BLUE){
+                return "Синий";
+            } else {
+                return "Оранжевый";
+            }
+        }
+    }
+}
