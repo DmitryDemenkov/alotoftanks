20a4079e29e019eae025f0a26f73f55f16904a1f
DmitryDemenkov <demon.krut2002@mail.ru>
2023-04-23 12:06:18 +0300

    added image utils class

---
+++ main/src/view/utils/ImageUtils.java
@@ -0,0 +1,44 @@
+package view.utils;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class ImageUtils {
+
+    public static BufferedImage resizeImage(BufferedImage img, Integer width, Integer height) {
+        Image tmpImg = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D g = bufferedImage.createGraphics();
+        g.drawImage(tmpImg, 0, 0, null);
+        g.dispose();
+
+        return bufferedImage;
+    }
+
+    public static BufferedImage rotateImage(BufferedImage img, double degree){
+        double angle = Math.toRadians(degree);
+
+        return tilt(img, angle);
+    }
+
+    public static BufferedImage tilt(BufferedImage image, double angle) {
+        double sin = Math.abs(Math.sin(angle)), cos = Math.abs(Math.cos(angle));
+        int w = image.getWidth(), h = image.getHeight();
+        int neww = (int)Math.floor(w*cos+h*sin), newh = (int)Math.floor(h*cos+w*sin);
+        GraphicsConfiguration gc = getDefaultConfiguration();
+        BufferedImage result = gc.createCompatibleImage(neww, newh, Transparency.TRANSLUCENT);
+        Graphics2D g = result.createGraphics();
+        g.translate((neww-w)/2, (newh-h)/2);
+        g.rotate(angle, w/2, h/2);
+        g.drawRenderedImage(image, null);
+        g.dispose();
+        return result;
+    }
+
+    public static GraphicsConfiguration getDefaultConfiguration() {
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice gd = ge.getDefaultScreenDevice();
+        return gd.getDefaultConfiguration();
+    }
+}
